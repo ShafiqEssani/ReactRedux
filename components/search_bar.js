ce41084d67cc5c01245e82d34b70b777a634faa0
Shafiq Essani <shafiqshamsessani@gmail.com>
2018-07-17 01:53:37 +0500

    All set with input tag

---
+++ components/search_bar.js
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { keyword: ''};
+    }
+
+    render() {
+    return (
+        <div>
+            <input 
+                value = {this.state.keyword}    
+                onChange = { (ev) => {
+                console.log(ev.target.value);
+                this.setState({ keyword: ev.target.value})
+        }}/>
+        </div>
+    );
+    }
+
+    // onInputChange(ev) {
+    //     console.log(ev.target.value)
+    // }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from '../components/search_bar';
 
 const API_KEY = 'AIzaSyBZoIhm4qBJJbu9qvTTkGRlPnb-zE27_b4';
 
 //creating new compo and this is going to produce some HTML
 const App = () => {
-    return <div> HI!</div>
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
-
 
 //Taking this compo html and put it in the dom
 
