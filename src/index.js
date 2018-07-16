fdd0bae010831db960be0343f7e494c79f285026
Shafiq Essani <shafiqshamsessani@gmail.com>
2018-07-17 02:09:59 +0500

    done with setting the state of videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+
 import SearchBar from '../components/search_bar';
 
 const API_KEY = 'AIzaSyBZoIhm4qBJJbu9qvTTkGRlPnb-zE27_b4';
 
 //creating new compo and this is going to produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'test'}, (videos) => {
+            this.setState({ videos });
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );        
+    }
+
 }
 
 //Taking this compo html and put it in the dom
